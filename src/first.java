3ddd6067ae5013f55d5e0bc2bfb87b090be4dd28
Prashant <prshantsemail@gmail.com>
2021-01-10 23:52:09 +0530

    first commit

---
+++ src/first.java
@@ -0,0 +1,27 @@
+import sun.reflect.generics.tree.Tree;
+
+import java.util.*;
+public class first {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String w = sc.nextLine();
+        String arr[] = w.split(" ");
+        Map<Integer,Integer> weight = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            int num = Integer.parseInt(arr[i]);
+            String temp[] = arr[i].split("");
+            int sum = 0;
+            for(int j = 0; j < temp.length; j++){
+                sum += Integer.parseInt(temp[j]);
+            }
+            weight.put(num,sum);
+        }
+
+
+
+    }
+}
+
+
+
+
---
+++ src/learn.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+class pair
+{
+    int first;
+    int second;
+    pair(int first, int second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString(){
+        return this.first + " ," + this.second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+//        if (this == o) return true;
+        if (!(o instanceof pair)) return false;
+        pair pair = (pair) o;
+        return first == pair.first &&
+                second == pair.second;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+}
+class learn {
+    public static void overlappedInterval(ArrayList<pair> vec, int n) {
+        Set<pair> set = new HashSet<pair>();
+
+        for (int i = 0; i < n; i++) {
+            int small = vec.get(i).first;
+            int large = vec.get(i).second;
+            pair temp = new pair(0,0);
+            for(int j = i+1; j < n; j++){
+                if(vec.get(j).first < large && vec.get(j).second < large){
+                    small = vec.get(i).first;
+                    large = vec.get(i).second;
+                }
+                else if(vec.get(j).first < large && vec.get(j).second > large){
+                    small = vec.get(i).first;
+                    large = vec.get(j).second;
+                }
+                temp.first = small;
+                temp.second = large;
+                set.add(temp);
+
+            }
+
+        }
+        for(int i = 0; i < set.size(); i++){
+//            set.equals()
+            System.out.println(set.toString());
+        }
+        // code here
+    }
+
+    public static void main(String[] args) {
+        ArrayList<pair> list = new ArrayList<>();
+        pair obj = new pair(6,8);
+        pair obj1 = new pair(1,9);
+        pair obj2 = new pair(2,4);
+        pair obj3 = new pair(4,7);
+        list.add(obj);
+        list.add(obj1);
+        list.add(obj2);
+        list.add(obj3);
+        overlappedInterval(list,list.size());
+    }
+}
+
